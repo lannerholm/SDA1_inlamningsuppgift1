326ba4159167de490109324f22935bc11302b168
Daniel Lännerholm Palm <daniel.lannerholm@protonmail.com>
2023-01-22 10:08:42 +0100

    a

---
+++ daniels_utkast.R
@@ -0,0 +1,5 @@
+setwd("/home/daniel/statistics/SDA1/SDA1_inlamningsuppgift1")
+install.packages(c("mosaic", "dplyr", "geosphere", "leaflet"))
+load(file = url("https://github.com/StatisticsSU/SDA1/blob/main/assignments/assignment1/Boston_census_data.RData?raw=true")) 
+load(file = url("https://github.com/StatisticsSU/SDA1/blob/main/assignments/assignment1/Boston_districts_to_predict.RData?raw=true"))
+# a comment
